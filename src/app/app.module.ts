f257f8b4ebbb3a7da748445d74d138827cbd0849
ElianMelo <elianmelomorais@gmail.com>
2022-10-17 21:47:36 -0300

    Configurando as rotas da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { HomeModule } from './home/home.module';
   ],
   imports: [
     BrowserModule,
-    HomeModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
